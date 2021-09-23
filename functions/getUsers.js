de30091cfc705ef889e5d0705301fd08e712391d
Muhammad Ahsan <muhammedahsan121@gmail.com>
2021-09-23 00:14:23 -0700

    event edited

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -2,14 +2,15 @@ const axios = require("axios");
  
 exports.handler = async function (event, context, callback) {
   const data = await axios("https://api.publicapis.org/entries");
+  
   console.log(context, "Muhammad Ahsan");
   if (event.httpMethod == "GET") {
-  
       callback(null, {
       statusCode: 200, 
       body: JSON.stringify({
         msg: "Following is the api response from the public animal names apis ",
-        data: data.data,
+        // data: data.data,
+        data:event.body
       }),
     });
   } else if (event.httpMethod == "POST") {
